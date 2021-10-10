cef99e579b3e16c0c785cef96dc62c44c094a45d
Christian Welham <welhamm@gmail.com>
2021-10-10 15:59:09 +0100

    add user avatar and styling

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def avatar_for(user)
+    hash = Digest::MD5::hexdigest(user.email.downcase)
+    url  = "https://secure.gravatar.com/avatar/#{hash}"
+    image_tag(url, alt:user.name, class:"avatar")
+  end
 end
