0eb86d43977cb80ccc66708bb54bb2e319d3c0dd
Christian Welham <welhamm@gmail.com>
2021-10-10 01:45:44 +0100

    add user field presence validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "creating a user" do
+    def create(name:nil, email:nil)
+      User.new(name:name, email:email)
+    end
+    describe "testing field presence" do
+      context "when name and email present" do
+        it "is valid" do
+          user = create(name:"example", email:"a@b.com")
+          expect(user.valid?).to be(true)
+        end
+      end
+      context "when name is omitted" do
+        it "is invalid" do
+          user = create(email:"a@b.com")
+          expect(user.valid?).to_not be(true)
+        end
+      end
+      context "when email is omitted" do
+        it "is invalid" do
+          user = create(name:"example")
+          expect(user.valid?).to_not be(true)
+        end
+      end
+    end
+  end
 end
