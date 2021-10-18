b1b70a3d740f455559e7968e4e10e9e8f249324c
Christian Welham <welhamm@gmail.com>
2021-10-18 05:20:29 +0100

    implement password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,13 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,         only: [:edit, :update]
+  before_action :check_user,       only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
   def new
   end
+  def edit
+  end
+
   def create
     @user = User.find_by(email:params[:password_reset][:email].downcase)
     if @user
@@ -13,6 +20,40 @@ class PasswordResetsController < ApplicationController
       render "new"
     end
   end
-  def edit
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render "edit"
+    elsif @user.update(user_params)
+      reset_session
+      log_in @user
+      flash[:success] = "Password has been reset"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
+
+  private
+  def get_user
+    @user = User.find_by(email:params[:email])
+  end
+  def check_user
+    redirect_to root_url unless valid_reset?
+  end
+  def valid_reset?
+    @user && @user.activated? && 
+      @user.authenticated?(:reset, params[:id])
+  end
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset has expired"
+      redirect_to new_password_reset_url
+    end
+  end
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -51,7 +51,7 @@ class User < ApplicationRecord
 	def create_reset_digest
 		@reset_token = self.class.new_token
 		update_attribute(:reset_digest, self.class.digest(reset_token))
-		update_attribute(:reset_sent_at, Time.zone.new)
+		update_attribute(:reset_sent_at, Time.zone.now)
 	end
 
 	def send_activation_email
@@ -60,6 +60,10 @@ class User < ApplicationRecord
 
 	def send_password_reset_email
 		UserMailer.password_reset(self).deliver_now
+	end
+
+	def password_reset_expired?
+		reset_sent_at < 2.hours.ago
 	end
 
 	private
