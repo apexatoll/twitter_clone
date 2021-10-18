a88470cca1796fd6d4f4930793aac0260bcdb664
Christian Welham <welhamm@gmail.com>
2021-10-18 21:17:48 +0100

    add micropost controller and log in validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
-  def hello
-    render html:"hello world"
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in"
+      redirect_to login_url
+    end
   end
 end
--- spec/requests/microposts_spec.rb
+++ spec/requests/microposts_spec.rb
@@ -1,7 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "Microposts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /microposts" do
+    context "when not logged in" do
+      def send_post(content:"Lorem ipsum")
+        post microposts_path, params:{ micropost:{ content:content } }
+      end
+      it "redirects to login page" do
+        send_post
+        expect(response).to redirect_to(login_url)
+      end
+      it "does not post a new micropost" do
+        expect{ send_post }.to_not change{ Micropost.count }
+      end
+    end
+  end
+  describe "DELETE /microposts/:id" do
+    fixtures :microposts
+    let(:micropost){ microposts(:hello_world) }
+    def delete_post
+      delete micropost_path(micropost)
+    end
+    it "redirects to login page" do
+      delete_post
+      expect(response).to redirect_to(login_url)
+    end
+    it "does not delete the post" do
+      expect{ delete_post }.to_not change{ Micropost.count }
+    end
   end
 end
