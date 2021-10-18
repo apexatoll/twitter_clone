39bc30fe6fa8d88e5f47cd699826ab42f2c4a32a
Christian Welham <welhamm@gmail.com>
2021-10-18 21:54:51 +0100

    implement new micropost form on homepage

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,9 +1,23 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only:[:create, :destroy]
   def create
-
+    @micropost = make
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render "static_pages/home"
+    end
   end
   def destroy
 
+  end
+
+  private
+  def make
+    current_user.microposts.build(micropost_params)
+  end
+  def micropost_params
+    params.require(:micropost).permit(:content)
   end
 end
